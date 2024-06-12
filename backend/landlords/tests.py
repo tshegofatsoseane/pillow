851fca75a84f20d21039ce60d40de8f7607f1d5f
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-06-13 01:08:00 +0200

    [Update] - Added one testcase for Landlords

--- backend/landlords/tests.py
+++ backend/landlords/tests.py
@@ -1,3 +1,22 @@
+from rest_framework.test import APIClient
 from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Landlord
 
-# Create your tests here.
+
+class LandlordAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.register_url = reverse('landlord-register')
+        self.login_url = reverse('landlord-login')
+        
+        self.user_data = {
+            'email': 'jdoe@yahoo.com',
+            'password': 'my_password'
+        }
+        
+    def test_register(self):
+        response = self.client.post(self.register_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
